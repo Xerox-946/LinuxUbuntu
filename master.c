3996e91efc33d0f392a7b7e7a33c84d0b6f30e09
ccc <xerox946@163.com>
2023-03-28 09:00:13 +0800

    xiaozhang

--- master.c
+++ master.c
@@ -9,19 +9,19 @@
 void m_init()
 {
 	
-	char c[10];//测试指针的位置，因为打开都是在0位置，所以用一个数组判断文件是否为空
+	char c[10];//测试指针的位置，因为打开时，文件指针都是在0位置，所以用一个数组判断文件是否为空
 	
 	mfile=fopen("master.bin","a+");
 	if(NULL==mfile)
 	{
 		perror("fopen");
+		anykey_continue();
 		return;
 	}
-	//printf("\t\t\t%ld\n",ftell(mfile));//测试指针的位置
-	if(0==fread(c,sizeof(c),1,mfile))
+	if(0==fread(c,sizeof(c),1,mfile))//文件为空则导入数据
 	{
 		
-		char p[6]="admin";
+		char p[7]="admin";
 		char p_m[7]="123456";
 		strcpy(m,p);
 		strcpy(m_m,p_m);	
@@ -30,7 +30,7 @@ void m_init()
 		fclose(mfile);
 		
 	}
-	else
+	else//文件非空则写入数组
 	{
 		rewind(mfile);
 		fread(m,sizeof(m),1,mfile);
@@ -45,21 +45,21 @@ void m_init()
 void master(void)
 {
 	m_init();
-	if(mfile==NULL)
+	if(mfile==NULL)//判定是什么状态回来的，如果是提前return回来，那么是出现错误，不应该往下执行.正常关闭后mfike指向-1
 	{
+		fclose(mfile);
 		return;
 	}
-	
 	int i=m_login_view();
 	if(0==i)
 	{
-		m_newpassword_view();//新密码界面------------------------------
+		m_newpassword_view();//新密码界面
 	}
 	else
 	{
-		m_success_view();
+		m_success_view();//成功登陆界面
 	}	
-	tfile=fopen("teacher.bin","r+");
+	tfile=fopen("teacher.bin","r+");//为了减少文件的读取次数，只有关键的校长操作用到了即时存储，教师和学生在系统关闭后才会存储数据。为了让文件打开关闭一致，所以需要在最后再打开文件
 	
 }
 
