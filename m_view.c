6a83a94ddb2f1e7de2e9dfc2b77efb6bec230bc4
ccc <xerox946@163.com>
2023-03-29 10:33:17 +0800

    xiaozhang

--- m_view.c
+++ m_view.c
@@ -12,31 +12,29 @@
 int m_login_view(void)
 {
 	
-	char my_m[7]={};//用户名
-	char m_password[7]={};//输入的密码
+	char my_m[20]={};//用户名
+	char m_password[16]={};//输入的密码
 	
 	
-	char arr2[7]="123456";//判断是否是第一次进入
+	char arr2[16]="123456";//判断是否是第一次进入
 	while(1)
 	{
 		system("clear");
-		if(!strcmp(arr2,m_m))
+		if(!strcmp(arr2,m_pwd))
 		{
-			printf("初始密码是123456\n");
-	
+			printf("密码为初始密码\n");
 			printf("请输入校长账户名");
-			fgets(my_m,7,stdin);	//数组长度要比用户名长两个字节，因为在fgets会接收回车键
+			fgets(my_m,22,stdin);	//数组长度要比用户名长两个字节，因为在fgets会接收回车键
 			control_write(my_m);
 			printf("请输入校长密码");
 			pwdstr(m_password);
-			printf("%s\n",m_password);
 	
-			if(strcmp(m,my_m)!=0)
+			if(strcmp(m_count,my_m)!=0)
 			{
 				printf("账户名有误,请重新输入\n");
 				anykey_continue();
 			}
-			else if(strcmp(m_password,m_m)!=0)
+			else if(strcmp(m_password,m_pwd)!=0)
 			{
 				printf("密码有误，请重新输入\n");
 				anykey_continue();
@@ -49,35 +47,34 @@ int m_login_view(void)
 			}
 		
 		}
-	else
-	{
-		printf("请输入校长账户名");
-			fgets(my_m,7,stdin);
-			control_write(my_m);
-			printf("请输入校长密码");
-			
-			pwdstr(m_password);
-		if(strcmp(m,my_m)!=0)
-		{
-			printf("账户名有误\n");
-			anykey_continue();
-			
-		}
-		else if(strcmp(m_password,m_m)!=0)
-		{
-			printf("密码有误\n");
-			anykey_continue();
-		
-		}
 		else
 		{
-			printf("登录成功\n");
-			anykey_continue();
-			return 2;//不是第一次登录
+			printf("请输入校长账户名");
+			fgets(my_m,22,stdin);
+			control_write(my_m);
+			printf("请输入校长密码");	
+			pwdstr(m_password);
+			if(strcmp(m_count,my_m)!=0)
+			{
+				printf("账户名有误\n");
+				anykey_continue();
+			
+			}
+			else if(strcmp(m_password,m_pwd)!=0)
+			{
+				printf("密码有误\n");
+				anykey_continue();
+		
+			}
+			else
+			{
+				printf("登录成功\n");
+				anykey_continue();
+				return 2;//不是第一次登录
+			}
+		
+		
 		}
-		
-		
-	}
 		
 		
 	}
@@ -87,44 +84,42 @@ int m_login_view(void)
 
 void m_success_view()
 {
-while(1){
-	system("clear");
-	printf("\t\t欢迎登录学生管理系统校长操作界面\n\n");
-	printf("\t\t\t按1重置密码\n\n");
-	printf("\t\t\t按2重置教师密码\n\n");
-	printf("\t\t\t按3添加教师\n\n");
-	printf("\t\t\t按4删除教师\n\n");
-	printf("\t\t\t按5显示所有在职教师\n\n");
-	printf("\t\t\t按6显示所有离职教师\n\n");
-	printf("\t\t\t按7解锁教师帐号\n\n");
-	printf("\t\t\t按8退出登录\n\n");
-
-	int i=getch();
-	switch(i)
+	while(1)
 	{
-	case '1':m_reself_password();break;
-	case '2':m_reteacher_password();break;
-	case '3':m_add_teacher();break;
-	case '4':m_del_teacher();break;
-	case '5': m_show_workteacher();break;
-	case '6':m_show_fireteacher();break;
-	case '7':m_open_teacher_count();break;
-	case '8':return; break;
-	}
-	}
-	
-	
-	
+		system("clear");
+		printf("\t\t欢迎登录学生管理系统校长操作界面\n\n");
+		printf("\t\t\t按1重置密码\n\n");
+		printf("\t\t\t按2重置教师密码\n\n");
+		printf("\t\t\t按3添加教师\n\n");
+		printf("\t\t\t按4删除教师\n\n");
+		printf("\t\t\t按5显示所有在职教师\n\n");
+		printf("\t\t\t按6显示所有离职教师\n\n");
+		printf("\t\t\t按7解锁教师帐号\n\n");
+		printf("\t\t\t按8退出登录\n\n");
+
+		int i=getch();
+		switch(i)
+		{
+		case '1':m_reself_password();break;
+		case '2':m_reteacher_password();break;
+		case '3':m_add_teacher();break;
+		case '4':m_del_teacher();break;
+		case '5': m_show_workteacher();break;
+		case '6':m_show_fireteacher();break;
+		case '7':m_open_teacher_count();break;
+		case '8':return; break;
+		}
+	}	
 }
 
 
 void m_newpassword_view()
 {
-	char m_newpassword[7];//新密码
-	char m_newpassword2[7];//密码的二次确认
+	char m_newpassword[16];//新密码
+	char m_newpassword2[16];//密码的二次确认
 	int i=1;
 	system("clear");
-	printf("请设置新的六位数密码");
+	printf("请设置新的密码");
 		while(i)
 		{
 			pwdstr(m_newpassword);
@@ -132,7 +127,7 @@ void m_newpassword_view()
 			pwdstr(m_newpassword2);
 			if(0==strcmp(m_newpassword2,m_newpassword))
 			{
-				strcpy(m_m,m_newpassword);
+				strcpy(m_pwd,m_newpassword);
 				printf("密码设置成功,请重新登录\n");
 				i=0;
 			}
@@ -149,8 +144,8 @@ void m_newpassword_view()
 			perror("fopen");
 			return;
 		}
-		fwrite(m,sizeof(m),1,mfile);
-		fwrite(m_m,sizeof(m_m),1,mfile);
+		fwrite(m_count,sizeof(m_count),1,mfile);
+		fwrite(m_pwd,sizeof(m_pwd),1,mfile);
 		fclose(mfile);
 }
 
--- master.c
+++ master.c
@@ -4,12 +4,13 @@
 #include <getch.h>
 #include "m_view.h"
 
-
+char m_count[20];
+char m_pwd[16];
 
 void m_init()
 {
 	
-	char c[10];//测试指针的位置，因为打开时，文件指针都是在0位置，所以用一个数组判断文件是否为空
+	char temp[10];//测试指针的位置，因为打开时，文件指针都是在0位置，所以用一个数组判断文件是否为空
 	
 	mfile=fopen("master.bin","a+");
 	if(NULL==mfile)
@@ -18,23 +19,23 @@ void m_init()
 		anykey_continue();
 		return;
 	}
-	if(0==fread(c,sizeof(c),1,mfile))//文件为空则导入数据
+	if(0==fread(temp,sizeof(temp),1,mfile))//文件为空则导入数据
 	{
 		
-		char p[7]="admin";
-		char p_m[7]="123456";
-		strcpy(m,p);
-		strcpy(m_m,p_m);	
-		fwrite(p,sizeof(p),1,mfile);		
-		fwrite(p_m,sizeof(p_m),1,mfile);	
+		char p_count[20]="admin";
+		char p_pwd[16]="123456";
+		strcpy(m_count,p_count);
+		strcpy(m_pwd,p_pwd);	
+		fwrite(p_count,sizeof(p_count),1,mfile);		
+		fwrite(p_pwd,sizeof(p_pwd),1,mfile);	
 		fclose(mfile);
 		
 	}
 	else//文件非空则写入数组
 	{
 		rewind(mfile);
-		fread(m,sizeof(m),1,mfile);
-		fread(m_m,sizeof(m_m),1,mfile);
+		fread(m_count,sizeof(m_count),1,mfile);
+		fread(m_pwd,sizeof(m_pwd),1,mfile);
 		fclose(mfile);
 	}
 	
@@ -47,11 +48,10 @@ void master(void)
 	m_init();
 	if(mfile==NULL)//判定是什么状态回来的，如果是提前return回来，那么是出现错误，不应该往下执行.正常关闭后mfike指向-1
 	{
-		fclose(mfile);
 		return;
 	}
-	int i=m_login_view();
-	if(0==i)
+	int temp=m_login_view();
+	if(0==temp)
 	{
 		m_newpassword_view();//新密码界面
 	}
@@ -60,6 +60,11 @@ void master(void)
 		m_success_view();//成功登陆界面
 	}	
 	tfile=fopen("teacher.bin","r+");//为了减少文件的读取次数，只有关键的校长操作用到了即时存储，教师和学生在系统关闭后才会存储数据。为了让文件打开关闭一致，所以需要在最后再打开文件
-	
+	if(NULL==tfile)
+	{
+		perror("tfile");
+		anykey_continue();
+		return;
+	}
 }
 
